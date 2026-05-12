a99f546751e2b29385165cb2c0daa13fdd193278
fadli-wq <dayfos123@gmail.com>
2026-05-12 17:08:12 +0700

    Add secret DB installation route

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,13 @@ Route::get('/emiten/compare', [EmitenController::class, 'compare'])->name('emite
 Route::get('/emiten/{kode}', [EmitenController::class, 'show'])->name('emiten.show');
 Route::get('/sektor', [SectorController::class, 'index'])->name('sector.index');
 Route::get('/sektor/{slug}', [SectorController::class, 'show'])->name('sector.show');
+
+// TODO: Hapus rute ini setelah berhasil dijalankan di server!
+Route::get('/install-db-secret', function () {
+    try {
+        \Illuminate\Support\Facades\Artisan::call('migrate:fresh', ['--seed' => true, '--force' => true]);
+        return "Berhasil! Database MySQL telah di-reset dan diisi ratusan data. <br><br>Log Output:<br>" . nl2br(\Illuminate\Support\Facades\Artisan::output());
+    } catch (\Exception $e) {
+        return "Gagal: " . $e->getMessage();
+    }
+});
